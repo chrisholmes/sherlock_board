73edf5b23bae66c251b5238d70377c89533e6dec
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2017-04-23 12:33:53 +0100

    foo

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -54,9 +54,14 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+let boxes = $('.box')
+boxes.each(function(box) {
+  let job = $(this).attr('job')
+  if(job != null) {
+    let channel = socket.channel("jobs:my_job", {})
+    channel.on("event", payload => {
+    }) 
+  }
 
+})
 export default socket
