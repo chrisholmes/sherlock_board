8064bb9c4a3e1d5952bddd62f08d4edc93d40184
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2017-04-25 21:28:51 +0100

    split MyJob into HtmlJob and EventJob

---
+++ jobs/event_job.ex
@@ -0,0 +1,9 @@
+defmodule SherlockBoard.EventJob do
+  import SherlockBoard.Job
+
+  def run do
+    send_event("event", %{value: "bar"})
+  end
+
+  def period, do: 2
+end
--- jobs/html_job.ex
+++ jobs/html_job.ex
@@ -1,9 +1,8 @@
-defmodule SherlockBoard.MyJob do
+defmodule SherlockBoard.HtmlJob do
   import SherlockBoard.Job
 
   def run do
     send_html("html","<p>Hello World</p>")
-    send_event("event", %{value: "bar"})
   end
 
   def period, do: 2
