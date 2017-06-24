5f843f271d6a8da67223dda61cbdd44e3bd54cbf
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2017-06-24 20:12:47 +0100

    configure timeouts

--- example_dashboard/config/test.exs
+++ example_dashboard/config/test.exs
@@ -8,4 +8,4 @@ config :sherlock_board, SherlockBoard.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
-config :hound, driver: "selenium"
+config :hound, driver: "selenium", http: [recv_timeout: 60000, timeout: 60000]
