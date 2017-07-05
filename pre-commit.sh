a441bfcf94c00625d8f1b857e6f3b96c89f50a53
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2017-07-05 22:11:38 +0100

    ensure yarn and brunch are run

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,5 +2,5 @@
 set -eu
 set -o pipefail
 mix test
-(cd example_dashboard && mix deps.get && mix test)
-(cd installer && mix deps.get && mix test)
+(cd example_dashboard && mix deps.get && yarn install && brunch b && mix test)
+(cd installer && mix deps.get && yarn install && brunch b && mix test)
