92454efd79bb896a2f279af830e663b0ee7c10fb
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2017-07-07 23:10:47 +0100

    only install dependencies explicitly on travis

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,5 +2,5 @@
 set -eu
 set -o pipefail
 mix test
-(cd example_dashboard && mix deps.get && yarn install && brunch b && mix test)
-(cd installer && mix deps.get && yarn install && brunch b && mix test)
+(cd example_dashboard && mix test)
+(cd installer && mix test)
